c85b9a0dd99598a839fe3df97a8b990456009f77
Adrian Bona <adrianbona@outlook.com>
2024-04-25 10:27:54 -0300

    Reviews and fixes Ex 1

--- src/Guide_2.hs
+++ src/Guide_2.hs
@@ -13,7 +13,7 @@ module Guide_2 (
 -- Ejercicio 1
 
 intercambiar :: (a,b) -> (b,a)
-intercambiar (x,y) = (y,x) --{INT}
+intercambiar (x,y) = (y,x) --{INT0}
 
 espejar :: Either a b -> Either b a
 espejar (Left x) = Right x --{E1}
@@ -40,6 +40,7 @@ asociarD ((x,y),z) = (x,(y,z)) --{AD}
 -- id :: a -> a
 -- id x = x {ID}
 
+
 -- Extensionalidad para pares
 -- Si p :: (a, b), entonces ∃x :: a. ∃y :: b. p = (x, y).
 -- data Either a b = Left a | Right b
@@ -52,45 +53,49 @@ asociarD ((x,y),z) = (x,(y,z)) --{AD}
 
 -- i. ∀ p::(a,b) . intercambiar (intercambiar p) = p
 
--- intercambiar (intercambiar (x,y)) por {reemplazo}
--- intercambiar (y,x) por {INT}
--- (x,y) por {INT}
--- p {queda demostrada la igualdad}
+-- intercambiar (intercambiar p) = p {extensionalidad}
+-- intercambiar (intercambiar (x,y)) = p {INT0}
+-- intercambiar (y,x) = p {INT0}
+-- (x,y) = p {extensionalidad}
+-- p = p {queda demostrada la igualdad}
 
 -- ii. ∀ p::(a,(b,c)) . asociarD (asociarI p) = p
 
--- asociarD (asociarI (x,(y,z))) por {reemplazo}
--- asociarD ((x,y),z) {AI}
--- (x,(y,z)) {AD}
--- p {queda demostrada la igualdad}
+-- asociarD (asociarI p) = p {extensionalidad}
+-- asociarD (asociarI (x,(y,z))) = p {AI}
+-- asociarD ((x,y),z) = p {AD}
+-- (x,(y,z)) = p {extensionalidad}
+-- p = p {queda demostrada la igualdad}
 
 -- iii. ∀ p::Either a b . espejar (espejar p) = p
 
--- Caso donde: p = Left x
--- espejar (espejar (Left x)) {reemplazo}
--- espejar (Right x) {E1}
--- Left x {E1}
--- p {queda demostrada la igualdad}
+-- Si p::Either a b, entonces o bien ∃x :: a. e = Left x o bien ∃y :: b. e = Right b. Se consideran ambos casos.
 
--- Caso donde: p = Right x
--- espejar (espejar (Right x)) {reemplazo}
--- espejar (Left x) {E2}
--- Right x {E2}
--- p {queda demostrada la igualdad}
+-- Caso p = Left x
+-- espejar (espejar (p)) = p {extensionalidad}
+-- espejar (espejar (Left x)) = p {E1}
+-- espejar (Right x) = p {E2}
+-- Left x = p {extensionalidad}
+-- p = p {queda demostrada la igualdad}
+
+-- Caso p = Right x
+-- espejar (espejar (p)) = p {extensionalidad}
+-- espejar (espejar (Right x)) = p {E2}
+-- espejar (Left x) = p {E1}
+-- Right x = p {extensionalidad}
+-- p = p {queda demostrada la igualdad}
 
 -- iv. ∀ f::a->b->c . ∀ x::a . ∀ y::b . flip (flip f) x y = f x y
 
--- flip (flip f) x y
--- flip f y x por {F}
--- f x y {F}
--- f x y {queda demostrada la igualdad}
+-- flip (flip f) x y = f x y {F}
+-- flip f y x = f x y {F}
+-- f x y = f x y {queda demostrada la igualdad}
 
 -- v. ∀ f::a->b->c . ∀ x::a . ∀ y::b . curry (uncurry f) x y = f x y
 
--- curry (uncurry f) x y
--- uncurry f (x,y) {CU}
--- f x y {UC}
--- f x y {queda demostrada la igualdad}
+-- curry (uncurry f) x y = f x y {CU}
+-- uncurry f (x,y) = f x y {UC}
+-- f x y = f x y {queda demostrada la igualdad}
 
 -- Ejercicio 2
 
