ca72debba545cd10e8fd0f1c58bf431eb9d3d141
Adrian Bona <adrianbona@outlook.com>
2024-05-16 18:39:42 -0300

    Let's get this party started

--- src/Guide_5.hs
+++ src/Guide_5.hs
@@ -17,3 +17,15 @@ module Guide_5 (
 -- ø ⊢ λx. x ↪ ⟨x, x, ø⟩    x = ⟨(λy. if y then ff else tt) tt, ø⟩ ⊢ x ↪ ff
 ------------------------------------------------------------------------ MN
 -- ø ⊢ (λx. x) ((λy. if y then ff else tt) tt) ↪ ff
+
+
+-- Dar interpretación CBV para: (λx. x) ((λy. if y then ff else tt) tt)
+
+--                                                                                                                            ------------ TT    ------------- FF
+--                                                                                                                            y = tt ⊢ y ↪ tt    y = tt ⊢ ff ↪ ff
+--                                              -------- TT    ------------------------------------------------------- ABS    -------------------------------- IF
+--                                              ø ⊢ tt ↪ tt    ø ⊢ λy. if y then ff else tt ↪ ⟨y, if y then ff else tt, ø⟩    y = tt ⊢ if y then ff else tt ↪ ff
+-------------------- ABS    ------------- FF    ------------------------------------------------------------------------------------------------------------- MN
+-- ø ⊢ λx. x ↪ ⟨x, x, ø⟩    x = ff ⊢ x ↪ ff     ø ⊢ (λy. if y then ff else tt) tt ↪ ff
+----------------------------------------------------------------------------------- MN
+-- ø ⊢ (λx. x) ((λy. if y then ff else tt) tt) ↪ ff
