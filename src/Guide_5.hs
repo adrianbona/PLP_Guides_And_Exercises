3c2d19319ce74e338393ce2a0bb52dfd57d75c0c
Adrian Bona <adrianbona@outlook.com>
2024-05-16 13:33:44 -0300

    Let's get this party started

--- src/Guide_5.hs
+++ src/Guide_5.hs
@@ -1,3 +1,19 @@
 module Guide_5 (
 ) where
 
+-- Ejercicio Extra 1
+
+-- Dar interpretación CBN para: (λx. x) ((λy. if y then ff else tt) tt)
+
+--                                                                                         -------- TT
+--                                                                                         ø ⊢ tt ↪ tt
+--                                                                                         --------------------    ------------------ FF
+--                                                                                         y = ⟨tt, ø⟩ ⊢ y ↪ tt    y = ⟨tt, ø⟩ ⊢ ff ↪ ff
+--                          ------------------------------------------------------- ABS    ----------------------------------------- IF
+--                          ø ⊢ λy. if y then ff else tt ↪ ⟨y, if y then ff else tt, ø⟩    y = ⟨tt, ø⟩ ⊢ if y then ff else tt ↪ ff
+--                          --------------------------------------------------------------------------------------------------- MN
+--                          ø ⊢ (λy. if y then ff else tt) tt ↪ ff
+-------------------- ABS    -------------------------------------------- MN
+-- ø ⊢ λx. x ↪ ⟨x, x, ø⟩    x = ⟨(λy. if y then ff else tt) tt, ø⟩ ⊢ x ↪ ff
+------------------------------------------------------------------------ MN
+-- ø ⊢ (λx. x) ((λy. if y then ff else tt) tt) ↪ ff
