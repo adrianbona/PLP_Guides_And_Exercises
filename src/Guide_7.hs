e1a2d8c87135e26df82581c2f1cbe98c32514bdb
Adrian Bona <adrianbona@outlook.com>
2024-06-13 18:17:21 -0300

    Adding new ones

--- src/Guide_7.hs
+++ src/Guide_7.hs
@@ -1,3 +1,64 @@
 module Guide_7 (
 ) where
 
+-- Ejericio 1
+
+-- Convertir a forma normal conjuntiva y luego a forma clausal
+
+-- i. P ⇒ P
+
+-- P ⇒ P
+-- ¬P v P
+-- {¬P, P}
+
+
+-- ii. (P ∧ Q) ⇒ P
+
+-- (P ∧ Q) ⇒ P
+-- ¬(P ∧ Q) v P
+-- ¬P v ¬Q v P
+-- {¬P, ¬Q, P}
+
+
+-- iii. (P ∨ Q) ⇒ P
+
+-- (P ∨ Q) ⇒ P
+-- ¬(P ∨ Q) v P
+-- (¬P ∧ ¬Q) v P
+-- (¬P v P) ∧ (¬Q v P)
+-- {¬P, P}, {¬Q, P}
+
+
+-- iv. ¬(P ⇔ ¬P)
+
+-- ¬(P ⇔ ¬P)
+-- ¬((P ⇒ ¬P) ∧ (¬P ⇒ P))
+-- ¬((¬P v ¬P) ∧ (¬¬P v P))
+-- ¬((¬P v ¬P) ∧ (P v P))
+-- ¬(¬P ∧ P)
+-- P v ¬P
+-- {P, ¬P}
+
+
+-- v. ¬(P ∧ Q) ⇒ (¬P ∨ ¬Q)
+
+-- ¬(P ∧ Q) ⇒ (¬P ∨ ¬Q)
+-- ¬¬(P ∧ Q) v (¬P ∨ ¬Q)
+-- (P ∧ Q) v (¬P ∨ ¬Q)
+-- P v (¬P ∨ ¬Q) ∧ Q v (¬P ∨ ¬Q)
+-- (P v ¬P ∨ ¬Q) ∧ (Q v ¬P ∨ ¬Q)
+-- (true ∨ ¬Q) ∧ (true v ¬P)
+-- true
+-- {}
+
+
+-- vi. (P ∧ Q) ∨ (P ∧ R)
+
+-- (P ∧ Q) ∨ (P ∧ R)
+-- (P ∧ (Q ∨ R))
+-- (P ∧ Q) ∨ (P ∧ R)
+-- {P, Q}, {P, R}
+
+
+
+
