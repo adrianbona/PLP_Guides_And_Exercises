bd0466d509f0a13f49fba1098256f9918b4d0f2b
Adrian Bona <adrianbona@outlook.com>
2024-06-21 18:24:27 -0300

    Not so fast

--- src/Guide_8.hs
+++ src/Guide_8.hs
@@ -1,3 +1,46 @@
 module Guide_8 (
 ) where
 
+-- Ejercicio 1
+
+-- padre(juan, carlos).
+-- padre(juan, luis).
+-- padre(carlos, daniel).
+-- padre(carlos, diego).
+-- padre(luis, pablo).
+-- padre(luis, manuel).
+-- padre(luis, ramiro).
+-- abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
+
+-- i. Resultado de la consulta abuelo(X, manuel).
+
+-- abuelo(X,manuel)
+-- padre(X,Z), padre(Z,manuel)
+-- padre(X,luis), padre(luis,manuel)
+-- X = juan
+
+
+-- ii. Definir el predicado hijo/2, hermano/2 y descendiente/2.
+
+-- hijo(X, Y) :- padre(Y, X).
+-- hermano(X,Y) :- padre(Z,X), padre(Z,Y), X \= Y.
+
+-- % Una persona Y es un descendiente de X si X es el padre de Y.
+-- descendiente(X,Y) :- padre(X,Y).
+
+-- % Una persona Y es un descendiente de X si X es el padre de Z y Z es un descendiente de Y.
+-- descendiente(X,Y) :- padre(X,Z), descendiente(Z,Y).
+
+
+-- iii. Consulta descendiente(Alguien, juan).
+
+-- descendiente(Alguien, juan)
+
+---- padre(Alguien, juan)
+------ false
+
+---- padre(Alguien, Z), descendiente(Z, juan)
+------ padre(juan, carlos), descendiente(Z, juan)
+-------- padre(Z, juan)
+---------- false
+-------- padre(Z,Z1), descendiente(Z1,juan)
