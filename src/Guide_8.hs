e57dee03863fa20edd7205beb11d32195f6b4860
Adrian Bona <adrianbona@outlook.com>
2024-06-22 15:58:41 -0300

    Not so fast

--- src/Guide_8.hs
+++ src/Guide_8.hs
@@ -47,3 +47,67 @@ module Guide_8 (
 -- ?- hermano(pablo, X) .
 -- X = manuel ;
 -- X = ramiro.
+
+
+-- Ejercicio 2
+
+-- vecino(X, Y, [X|[Y|Ls]]).
+-- vecino(X, Y, [W|Ls]) :- vecino(X, Y, Ls).
+
+
+-- i. Mostrar el árbol de búsqueda en Prolog para resolver "vecino(5,Y,[5,6,5,3])"
+
+-- ?- vecino(5, Y, [5,6,5,3]) .
+
+-- Call: (12) vecino(5, Y, [5, 6, 5, 3]) ?
+-- Exit: (12) vecino(5, 6, [5, 6, 5, 3]) ?
+---- Y = 6 ;
+
+-- Redo: (12) vecino(5, Y, [5, 6, 5, 3]) ?
+-- Exit: (12) vecino(5, 3, [5, 6, 5, 3]) ?
+-- Fail: (12) vecino(5, Y, [5, 6, 5, 3]) ?
+
+-- Call: (13) vecino(5, Y, [6, 5, 3]) ?
+-- Exit: (13) vecino(5, 3, [6, 5, 3]) ?
+-- Fail: (13) vecino(5, Y, [6, 5, 3]) ?
+
+-- Call: (14) vecino(5, Y, [5, 3]) ?
+-- Exit: (14) vecino(5, 3, [5, 3]) ?
+---- Y = 3 ;
+
+-- Redo: (14) vecino(5, Y, [5, 3]) ?
+-- Fail: (14) vecino(5, Y, [5, 3]) ?
+
+-- Call: (15) vecino(5, Y, [3]) ?
+-- Fail: (15) vecino(5, Y, [3]) ?
+
+-- Call: (16) vecino(5, Y, []) ?
+-- Fail: (16) vecino(5, Y, []) ?
+
+---- false.
+
+
+-- ii. Si se invierte el orden de las reglas, ¿los resultados son los mismos? ¿Y el orden de los resultados?
+
+-- La forma de resolver la consulta es diferente aunque los resultados serán los mismos pero en un orden distinto.
+-- Prolog resuelve primero la regla recursiva. De esa manera el único resultado es Y = 3. Luego, al hacer redo,
+-- por fail en la regla base, utiliza la segunda regla y se obtiene Y = 6.
+
+-- Ejercicio 3
+
+-- natural(0).
+-- natural(suc(X)) :- natural(X).
+
+-- menorOIgual(X, suc(Y)) :- menorOIgual(X, Y).
+-- menorOIgual(X,X) :- natural(X).
+
+-- i. Explicar la consulta "menorOIgual(0,X)"
+
+-- Unifica con la primer aparición de la regla menorOIgual(X, suc(Y)) y se llama recursivamente con menorOIgual(0, Y).
+-- El problema es que no termina de unificar con una regla base, por lo que entra en un loop infinito.
+
+-- iii. Corregir la definición de menorOIgual/2.
+
+-- menorOIgual(X, X) :- natural(X).
+-- menorOIgual(X, suc(Y)) :- menorOIgual(X, Y).
+
